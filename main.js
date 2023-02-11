0f6ee394a0dda9fb68c09620c6ffafe638296ef9
Pedro-Henrique-Salles <110946918+Pedro-Henrique-Salles@users.noreply.github.com>
2023-02-11 19:14:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+        console.log("meu primeiro projeto no console");
+        var first_name ="Ranbir";
+        var last_name ="Kapoor";
+        var fist_last=first_name.concat(last_name);
+        console.log(fist_last)
+        var a = 10;
+        var b = 5;
+        var add = a + b ;
+        console.log(add);
+        var c = 7;
+        var d = 3;
+        var add = c + d ;
+        console.log(add);
+        var e = 10;
+        var f = 20;
+        var add = e + f ;
+        console.log(add);
+        var g = 40;
+        var h = 60;
+        var add = g + h ;
+        console.log(add);
